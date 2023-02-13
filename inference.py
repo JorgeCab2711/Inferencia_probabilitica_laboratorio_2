247dd85ba0d037582163dada81d2d4566dd63946
Jorge Caballeros Pérez <cab20009@uvg.edu.gt>
2023-02-13 13:48:33 -0600

    .

--- inference.py
+++ inference.py
@@ -3,7 +3,20 @@ import numpy as np
 
 class Infer():
     def __init__(self):
+        self.events = {}
         pass
+    def add_event(self, event:str, prob: float):
+        if event not in self.events:
+            self.events[event] = prob
+        else:
+            print("Warning: Adding event that already exists.", 'Event Name:', event)
 
-    def define(A, B):
-        pass
+
+
+inf = Infer()
+inf.add_event('A',0.5)
+inf.add_event('B',0.5)
+
+print(inf.events)
+
+
